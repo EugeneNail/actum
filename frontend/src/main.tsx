5a5ad98c6b889caa1dc165f03f3402115b502013
Евгений Гвоздиков <96439713+EugeneNail@users.noreply.github.com>
2024-05-19 21:33:08 +0300

    #89 refactor: replace location state with collection fetching

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,7 @@ import NotFoundPage from "./page/not-found-page/not-found-page.tsx";
 import CollectionsPage from "./page/collection/collections-page/collections-page.tsx";
 import SaveCollectionPage from "./page/collection/save-collection-page/save-collection-page.tsx";
 import DefaultLayout from "./layout/default-layout.tsx";
-import {DeletePage} from "./page/delete-page/delete-page.tsx";
+import {DeleteCollectionPage} from "./page/collection/delete-collection-page/delete-collection-page.tsx";
 import Notification from "./component/notification/notification.tsx";
 import SaveActivityPage from "./page/activity/save-activity-page/save-activity-page.tsx";
 
@@ -27,7 +27,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
                     <Route path="/settings/collections" element={<CollectionsPage/>}>
                         <Route path="/settings/collections/new" element={<SaveCollectionPage/>}/>
                         <Route path="/settings/collections/:id" element={<SaveCollectionPage/>}/>
-                        <Route path="/settings/collections/:id/delete" element={<DeletePage/>}/>
+                        <Route path="/settings/collections/:id/delete" element={<DeleteCollectionPage/>}/>
                         <Route path="/settings/collections/activities/new" element={<SaveActivityPage/>}/>
                     </Route>
                 </Route>
