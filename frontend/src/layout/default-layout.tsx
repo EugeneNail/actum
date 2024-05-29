7221a21b9a9e3a9b2c0c070701b10c797bce0a10
Евгений Гвоздиков <96439713+EugeneNail@users.noreply.github.com>
2024-05-29 15:56:25 +0300

    #114 feat: delete all the components

--- frontend/src/layout/default-layout.tsx
+++ frontend/src/layout/default-layout.tsx
@@ -1,11 +1,9 @@
 import "./default-layout.sass"
 import {Outlet} from "react-router-dom";
-import Header from "../component/header/header.tsx";
 
 export default function DefaultLayout() {
     return (
         <div className="default-layout">
-            <Header/>
             <Outlet/>
         </div>
     )
