0b4000708561c63118a5a5fbfca11c3164d0ad81
Евгений Гвоздиков <96439713+EugeneNail@users.noreply.github.com>
2024-05-30 15:21:10 +0300

    #114 feat: remove page transition animation

--- frontend/src/routing.tsx
+++ frontend/src/routing.tsx
@@ -1,33 +1,25 @@
-import {BrowserRouter, Navigate, Route, Routes, useLocation} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Notification from "./component/notification/notification.tsx";
-import {CSSTransition, TransitionGroup} from "react-transition-group";
 import LoginPage from "./page/guest-page/login-page.tsx";
 import SignupPage from "./page/guest-page/signup-page.tsx";
-
-function AnimatedRoutes() {
-    const location = useLocation()
-
-    return (
-        <div className="animation-wrapper">
-            <TransitionGroup>
-                <CSSTransition key={location.key} classNames="slide" timeout={300}>
-                    <Routes location={location}>
-                        <Route path="/Login" element={<LoginPage/>}/>
-                        <Route path="/signup" element={<SignupPage/>}/>
-                        <Route path="/" element={<Navigate to="/records"/>}/>
-                        {/*<Route path="*" element={<Navigate to="/not-found"/>}/>*/}
-                    </Routes>
-                </CSSTransition>
-            </TransitionGroup>
-        </div>
-    )
-}
+import DefaultLayout from "./layout/default-layout.tsx";
+import CollectionsPage from "./page/collections/collections-page/collections-page.tsx";
+import SaveCollectionPage from "./page/collections/save-collection-page/save-collections-page.tsx";
 
 export default function Routing() {
     return (
         <Notification>
             <BrowserRouter>
-                <AnimatedRoutes/>
+                <Routes>
+                    <Route path="/login" element={<LoginPage/>}/>
+                    <Route path="/signup" element={<SignupPage/>}/>
+                    <Route element={<DefaultLayout/>}>
+                        <Route path="/collections" element={<CollectionsPage/>}/>
+                        <Route path="/collections/new" element={<SaveCollectionPage/>}/>
+                        <Route path="/collections/:id" element={<SaveCollectionPage/>}/>
+                    </Route>
+                    <Route path="/" element={<Navigate to="/records"/>}/>
+                </Routes>
             </BrowserRouter>
         </Notification>
     )
