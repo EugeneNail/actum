4eaeb14bafeda3c17360db7db0b4c16082427590
Евгений Гвоздиков <96439713+EugeneNail@users.noreply.github.com>
2024-05-30 16:20:06 +0300

    #114 feat: add colors to the Button component

--- frontend/src/component/button/button.tsx
+++ frontend/src/component/button/button.tsx
@@ -1,6 +1,7 @@
 import "./button.sass"
 import {ReactNode} from "react";
 import classNames from "classnames";
+import {Color} from "../../model/color.tsx";
 
 export enum ButtonStyle {
     Primary,
@@ -9,27 +10,32 @@ export enum ButtonStyle {
 
 type Props = {
     className?:string
+    color?: Color
+    submit?: boolean
     even?: boolean
     round?: boolean
     shadowed?: boolean
-    style: ButtonStyle
+    style?: ButtonStyle
     onClick: () => void
     children: ReactNode
 }
 
-export default function Button({className, even, round, shadowed, style, onClick, children}: Props) {
+export default function Button({className, color = Color.green, submit, even, round, shadowed, style = ButtonStyle.Primary, onClick, children}: Props) {
     className = classNames(
         "button",
         className,
         {even: even},
         {round: round},
         {shadowed: shadowed},
+        {green: color == Color.green},
+        {yellow: color == Color.yellow},
+        {red: color == Color.red},
         {primary: style == ButtonStyle.Primary},
         {secondary: style == ButtonStyle.Secondary}
     )
 
     return (
-        <button className={className} onClick={onClick}>
+        <button type={submit ? "submit" : "button"} className={className} onClick={onClick}>
             {children}
         </button>
     )
