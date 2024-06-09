2c87f26822a8a6a7efccd6270bd2048d10d94f5b
Евгений Гвоздиков <96439713+EugeneNail@users.noreply.github.com>
2024-06-09 15:03:23 +0300

    #137 fix: set correct color for the BackButton component

--- frontend/src/component/form/form-back-button.tsx
+++ frontend/src/component/form/form-back-button.tsx
@@ -1,10 +1,11 @@
 import "./form.sass"
 import Button, {ButtonStyle} from "../button/button.tsx";
 import Icon from "../icon/icon.tsx";
+import {Color} from "../../model/color.tsx";
 
 export default function FormBackButton() {
     return (
-        <Button even style={ButtonStyle.Secondary} onClick={() => window.history.back()} >
+        <Button even color={Color.Accent} style={ButtonStyle.Secondary} onClick={() => window.history.back()} >
             <Icon name="west" bold/>
         </Button>
     )
