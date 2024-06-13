3d12a74d306ca4e51caed525ff36f2032c184418
Евгений Гвоздиков <96439713+EugeneNail@users.noreply.github.com>
2024-06-13 21:55:23 +0300

    #22 build: add a deployment script

---
+++ .ops/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function execute_pipe() {
+  local commands=("$@")
+
+  for command in "${commands[@]}"; do
+    echo -e "\033[1;33m[ $command ]\033[0m"
+    if ! eval "$command" ; then
+      return 1
+    fi
+  done
+}
+
+commands=(
+  "git pull origin main"
+  "cd ./frontend"
+  "npm install"
+  "npm audit fix"
+  "npm run build"
+  "cd ../backend"
+  "go mod download"
+  "go clean -cache"
+  "go build -o ./main ./cmd/main/main.go"
+  "./main"
+)
+
+execute_pipe "${commands[@]}"
