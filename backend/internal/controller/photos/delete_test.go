efe0ccb744ccdf5930ac5722c159a4f985754d2d
Евгений Гвоздиков <96439713+EugeneNail@users.noreply.github.com>
2024-06-13 11:12:37 +0300

    #151 test (photos): add tests for the Destroy route

---
+++ backend/internal/controller/photos/delete_test.go
@@ -0,0 +1,51 @@
+package photos
+
+import (
+	"fmt"
+	"github.com/EugeneNail/actum/internal/service/fake"
+	"github.com/EugeneNail/actum/internal/service/tests/startup"
+	"net/http"
+	"testing"
+)
+
+func TestDestroy(t *testing.T) {
+	client, database, storage := startup.Photos(t)
+
+	var name string
+	client.
+		Post("/api/photos", fmt.Sprintf(`{
+			"image": "%s"
+		}`, fake.Base64Image())).
+		AssertStatus(http.StatusCreated).
+		ReadData(&name)
+
+	database.
+		AssertCount("photos", 1).
+		AssertHas("photos", map[string]any{
+			"id":      1,
+			"name":    name,
+			"user_id": 1,
+		})
+
+	storage.
+		AssertCount("/photos", 1).
+		AssertHas("/photos", name)
+
+	client.
+		Delete("/api/photos/" + name).
+		AssertStatus(http.StatusNoContent)
+
+	database.AssertCount("photos", 0)
+	storage.AssertCount("/photos", 0)
+}
+
+func TestDestroyNonexistent(t *testing.T) {
+	client, database, storage := startup.Photos(t)
+
+	database.AssertCount("photos", 0)
+	storage.AssertCount("/photos", 0)
+
+	client.
+		Delete("/api/photos/file.jpg").
+		AssertStatus(http.StatusNotFound)
+}
