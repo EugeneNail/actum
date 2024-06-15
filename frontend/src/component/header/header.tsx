3b28eb29d25e66a190ee5cdf5225bd13a595e841
Евгений Гвоздиков <96439713+EugeneNail@users.noreply.github.com>
2024-06-15 17:45:11 +0300

    #163 feat: redraw the Header component

--- frontend/src/component/header/header.tsx
+++ frontend/src/component/header/header.tsx
@@ -1,66 +1,26 @@
 import "./header.sass"
-import {NavLink, useNavigate} from "react-router-dom";
-import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
 import Icon from "../icon/icon.tsx";
 import Button from "../button/button.tsx";
 import {Color} from "../../model/color.tsx";
+import HeaderLink from "./header-link.tsx";
 
 export default function Header() {
     const navigate = useNavigate()
-    const [isVisible, setVisible] = useState(true)
-
-    useEffect(() => {
-        let timer: number
-
-        const handleScroll = () => {
-            setVisible(true)
-
-            if (timer) {
-                clearTimeout(timer)
-            }
-            timer = setTimeout(() => {
-                setVisible(false)
-            }, 3000)
-        }
-
-        window.addEventListener('wheel', handleScroll)
-        window.addEventListener('touchmove', handleScroll)
-
-        return () => {
-            window.removeEventListener('wheel', handleScroll)
-            window.removeEventListener('touchmove', handleScroll)
-            if (timer) {
-                clearTimeout(timer)
-            }
-        }
-    }, [])
 
     return (
-        <>
-            {isVisible && <header className="header">
-                <NavLink className="header-link green" to="/statistics">
-                    <Icon className="header-link__icon" name="bar_chart"/>
-                </NavLink>
-                <NavLink className="header-link red" to="/records">
-                    <Icon className="header-link__icon" name="post"/>
-                </NavLink>
-                <div className="header__placeholder">
-                    <div className="header__button-container">
-                        <Button className="header__button" color={Color.Accent} even round
-                                onClick={() => navigate("/records/new")}>
-                            <Icon className="header__button-icon" name="add" bold/>
-                        </Button>
-                    </div>
+        <header className="header">
+            <HeaderLink label="Отчеты" color={Color.Green} icon="bar_chart" to="/statistics"/>
+            <HeaderLink label="Записи" color={Color.Red} icon="post" to="/records"/>
+            <div className="header__placeholder">
+                <div className="header__button-container">
+                    <Button className="header__button" color={Color.Accent} even round onClick={() => navigate("/records/new")}>
+                        <Icon className="header__button-icon" name="add" bold/>
+                    </Button>
                 </div>
-                <NavLink className="header-link orange" to="/collections">
-                    <Icon className="header-link__icon" name="category"/>
-                </NavLink>
-                <NavLink className="header-link  accent" to="/menu">
-                    <Icon className="header-link__icon" bold name="more_horiz"/>
-                </NavLink>
-            </header>
-            }
-
-        </>
+            </div>
+            <HeaderLink label="Коллекции" color={Color.Orange} icon="category" to="/collections"/>
+            <HeaderLink label="Меню" color={Color.Accent} icon="more_horiz" to="/menu"/>
+        </header>
     )
 }
